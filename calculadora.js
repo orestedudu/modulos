a311edf0d1f51ba1e6a18c4e77ea844d3bded0ee
Dudu <oresteeduardo81@gmail.com>
2023-08-29 15:16:29 -0300

    versao com outras operaçoes

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,9 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var dividir = require("./modulos/dividir");
+var multiplicar = require("./modulos/multiplicar");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(dividir(20,90));
+console.log(multiplicar(20,90));
