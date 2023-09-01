3ed670a3e5529d8fd590e31e02a1faba89f429ab
calculadora <oresteeduardo81@gmail.com>
2023-09-01 09:03:54 -0300

    versao com mensagem

--- calculadora.js
+++ calculadora.js
@@ -2,8 +2,14 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
 var multiplicar = require("./modulos/multiplicar");
+var subtrair = require("./modulos/subtrair");
+var mensagem = require("./modulos/mensagem")
+var nome = "Dudu";
 
 console.log(somar(20,90));
 console.log(media(20,90));
 console.log(dividir(20,90));
 console.log(multiplicar(20,90));
+console.log(subtrair(20,90));
+console.log(mensagem(nome));
+
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem = function (nome) {
+    return "Ola, " + nome +". Seja bem vindo ao curso de Node .]" 
+  }; 
+  module.exports = mensagem;
